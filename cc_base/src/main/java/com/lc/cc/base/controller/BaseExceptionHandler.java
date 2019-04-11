bc7c84c91b92a3e76c12da8b6be452a1f8d8cf3a
wlccomeon <wlccomeon@163.com>
2019-04-11 19:16:23 +0800

    add common exception handler

---
+++ cc_base/src/main/java/com/lc/cc/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.lc.cc.base.controller;
+
+import com.lc.entity.Result;
+import com.lc.entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * controller异常处理基类
+ * @author wlc
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+	@ExceptionHandler(value = Exception.class)
+	@ResponseBody
+	public Result error(Exception e){
+		e.printStackTrace();
+		return new Result(false, StatusCode.ERROR,e.getMessage());
+	}
+
+
+}
